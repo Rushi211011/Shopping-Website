4357f100bde458d8fb3fdf40fa6d75e7154626f2
Rushi211011 <rushikeshgadekar2105@gmail.com>
2025-06-14 09:35:17 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9527693711"><i class="fas fa-phone"></i> 9568741254</a>
+         <a href="tel:9586245612"><i class="fas fa-phone"></i> 9527693711</a>
+         <a href="mailto:rushikeshgadekar2105@gmail.com"><i class="fas fa-envelope"></i>rushikesh.gadekar@mitwpu.edu.in</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> PUNE </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/harshchaudharynp" target="_blank"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="https://www.linkedin.com/in/rushikesh-gadekar-568077260" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="www.linkedin.com/in/rushikesh-gadekar-568077260/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/rushikesh-gadekar-568077260/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>RUSHIKESH GADEKAR</span> | all rights reserved!</div>
+
+</footer>
